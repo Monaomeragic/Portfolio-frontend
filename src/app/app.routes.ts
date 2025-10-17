3b8e709229d559fc8501a3d17c2c6bd0c9e4376f
Monaomeragic <114739168+Monaomeragic@users.noreply.github.com>
2025-10-18 01:08:00 +0200

    dodano our-story(jos slike) i our-team

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
+import { OurStory } from './components/our-story/our-story';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'our-story', component: Home }, // Placeholder, can be replaced with actual component
+  { path: 'our-story', component: OurStory },
   { path: 'faq', component: Home }, // Placeholder
   { path: 'contact', component: Home }, // Placeholder
 ];
--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -4,7 +4,6 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,   
-  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
@@ -13,6 +12,14 @@ export class Navbar {
 
   isActive(route: string): boolean {
     return this.activeLink === route;
-    
+  }
+
+  scrollToSection(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      const yOffset = 10; // height of navbar + padding
+      const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      window.scrollTo({ top: y, behavior: 'smooth' });
+    }
   }
 }
